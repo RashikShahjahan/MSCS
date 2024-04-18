30db021640d96828c7715f216314fd45678a55b4
Rashik Shahjahan <rashikshahjahan@protonmail.com>
2024-04-18 13:29:51 -0400

    h

--- app.py
+++ app.py
@@ -53,3 +53,8 @@ async def get_applications():
 async def count_applications():
     count = await collection.count_documents({})
     return {"total_applications": count}
+
+import uvicorn
+
+if __name__ == "__main__":
+    uvicorn.run("app:app", host="127.0.0.1", port=8000, log_level="info", reload=True)
